52c74d2b9054c47180062708872e2bc54cad014f
Rukayat Oluwaseyi Rauf <103194370+Ratafar22@users.noreply.github.com>
2024-06-12 15:19:53 +0200

    Update Database Design Mini Project.sql

--- Database Design Mini Project.sql
+++ Database Design Mini Project.sql
@@ -1,14 +1,14 @@
 /* SITUATION 
-A new client, the owner of the Maven Movies DVD rental busines has reached out to you for help restructuring their non-normalized database.*/
+A new client, the owner of the Maven Movies DVD rental business has reached out to you for help restructuring their non-normalized database.*/
 
 /* Question 1:
 Take a look at the mavenmoviesmini schema. What do you notice about it? How many tables are there?
-What does the data represent? WHat do you think of the current schema? */
+What does the data represent? What do you think of the current schema? */
 
 SELECT * FROM inventory_non_normalized;
 
-/* Answer: the schema contains a table named inventory non-normalized which has 4,581 rows and 13 columns. The table include information of films and stores.
-Also, there are lot of duplicated information in the datasets which need to be normalise. */
+/* Answer: the schema contains a table named inventory non-normalized which has 4,581 rows and 13 columns. The table includes information on films and stores.
+Also, there is a lot of duplicated information in the datasets which needs to be normalised. */
 
 /* Question 2:
 If you wanted to break out the data from the inventory_non_normalized table into multiple tables, how many tables do you think would be ideal? 
@@ -20,7 +20,7 @@ What would you name those tables?/*
 -- Store table using store_id, store_manager_last_name, store_address, store_city, store_district */
 
 /* Question 3:
-Based on your answer from questio #2, create a new shcema with the tables you think will best serve this dataset. */
+Based on your answer from question #2, create a new schema with the tables you think will best serve this dataset. */
 
 -- Create a schema named mavenmovies_normalised
 CREATE SCHEMA mavenmovies_normalised;
@@ -70,13 +70,13 @@ Make sure your tables have the proper primary keys defined and that applicable f
 Add any constraints you think should apply to the data as well (unique, non- NULL etc) */
 
 /* Answer: Below are the constraints asses to each table
--- The inventory_id in the inventory table used as the primary key while the store_id and the film_id are used as the foreign keys and Non- NULL constrainsts was added to all columns
--- The Film table: The film_id was used as the primary key and non-NULL constraint was added to all columns
--- Store table: the store_id was used as the primary key and non-NULL constraint was added to the columns. */
+-- The inventory_id in the inventory table is used as the primary key while the store_id and the film_id are used as the foreign keys and Non- NULL constraint was added to all columns
+-- The Film table: The film_id was used as the primary key and a non-NULL constraint was added to all columns
+-- Store table: the store_id was used as the primary key and a non-NULL constraint was added to the columns. */
 
 /* Question 6: Finally, after doing all technical work, write a brief summary of what you have done, in a way that your non-technical client can understand. Communicate what you did,
 and why your new schema design is better.*/  
 
 /* SUMMARY:
-A table Inventory_non_normalized has been broken down into three tables - inventory, films and store leading to the elimination of duplicate values and enabling a well defined relationship between 
+A table Inventory_non_normalized has been broken down into three tables - inventory, films and store leading to the elimination of duplicate values and enabling a well-defined relationship between 
 the tables using the primary and foreign keys. This will help in ensuring data integrity and reducing too much load on the database. Now the Schema is fully optimized! */ 
